43d2e00cf8cf53349700f2dca344652e895e802e
alexlu07 <alexlu314@gmail.com>
2026-09-24 00:19:25 -0400

    limit T1 tracking torque

--- src/booster_mjlab/tasks/tracking/config/t1/env_cfgs.py
+++ src/booster_mjlab/tasks/tracking/config/t1/env_cfgs.py
@@ -1,12 +1,23 @@
 """Booster T1 full-body motion tracking configuration."""
 
+import dataclasses as _dc
+from copy import deepcopy
+
 from booster_mjlab.robots import T1_ACTION_SCALE, get_t1_robot_cfg
-from booster_mjlab.robots.booster_k1.sensors import FootClearanceSensorCfg, FootSoleGridPatternCfg
+from booster_mjlab.robots.booster_k1.sensors import (
+    FootClearanceSensorCfg,
+    FootSoleGridPatternCfg,
+)
+from booster_mjlab.robots.booster_k1.torque_speed import (
+    BoosterEntityCfg,
+    BoosterPositionActuatorCfg,
+    initialize_torque_speed_limits,
+)
 from booster_mjlab.tasks.tracking.tracking_env_cfg import make_tracking_env_cfg
 from booster_mjlab.tasks.velocity.mdp import foot_height
 from mjlab.envs import ManagerBasedRlEnvCfg
 from mjlab.envs.mdp.actions import JointPositionActionCfg
-from mjlab.managers import ObservationTermCfg, RewardTermCfg
+from mjlab.managers import EventTermCfg, ObservationTermCfg, RewardTermCfg
 from mjlab.sensor import ContactMatch, ContactSensorCfg, ObjRef
 from mjlab.tasks.tracking import mdp
 from mjlab.tasks.tracking.mdp import MotionCommandCfg
@@ -16,9 +27,29 @@ T1_TRACKING_BODIES = (
 )
 
 
+def _get_t1_tracking_robot_cfg() -> BoosterEntityCfg:
+    """Use the K1 tracking torque-speed mechanism with T1 motor curves."""
+    base_robot_cfg = get_t1_robot_cfg()
+    robot_cfg = BoosterEntityCfg(
+        **{
+            f.name: getattr(base_robot_cfg, f.name)
+            for f in _dc.fields(base_robot_cfg)
+        }
+    )
+    robot_cfg.articulation = deepcopy(robot_cfg.articulation)
+    assert robot_cfg.articulation is not None
+    robot_cfg.articulation.actuators = tuple(
+        BoosterPositionActuatorCfg(
+            **{f.name: getattr(actuator, f.name) for f in _dc.fields(actuator)}
+        )
+        for actuator in robot_cfg.articulation.actuators
+    )
+    return robot_cfg
+
+
 def booster_t1_flat_tracking_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
     cfg = make_tracking_env_cfg()
-    cfg.scene.entities = {"robot": get_t1_robot_cfg()}
+    cfg.scene.entities = {"robot": _get_t1_tracking_robot_cfg()}
     self_collision = ContactSensorCfg(name="self_collision", primary=ContactMatch(mode="subtree", pattern="trunk", entity="robot"), secondary=ContactMatch(mode="subtree", pattern="trunk", entity="robot"), fields=("found",), reduce="none", num_slots=1)
     foot_scan = FootClearanceSensorCfg(name="foot_height_scan", frame=tuple(ObjRef(type="site", name=name, entity="robot") for name in ("left_foot_sole", "right_foot_sole")), pattern=FootSoleGridPatternCfg(size=(0.20, 0.08)), ray_alignment="yaw", max_distance=1.0, exclude_parent_body=True, include_geom_groups=(0,), debug_vis=True)
     cfg.scene.sensors = (self_collision, foot_scan)
@@ -26,6 +57,9 @@ def booster_t1_flat_tracking_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg
     action = cfg.actions["joint_pos"]
     assert isinstance(action, JointPositionActionCfg)
     action.scale = T1_ACTION_SCALE
+    cfg.events["initialize_torque_speed_limits"] = EventTermCfg(
+        mode="startup", func=initialize_torque_speed_limits
+    )
     assert cfg.commands is not None
     motion = cfg.commands["motion"]
     assert isinstance(motion, MotionCommandCfg)
